9f13f7c196dd4e95a6c8c27493c64a736e2bce0b
Matthias M. Fischer <33027321+Ma-Fi-94@users.noreply.github.com>
2023-12-05 16:50:44 +0100

    Part 1

--- day05/solution.hs
+++ day05/solution.hs
@@ -1,40 +1,27 @@
 import Utils (readInt, tok, at)
 import Data.List (sort)
 
------------
--- Sugar --
------------
-
+-- Sugar
 type Rule  = (Int, Int, Int) -- (src, dst, len) 
 type Table = [Rule]          -- a block from the input text
 
-------------
--- Part 1 --
-------------
-
 -- Parse a line of three numbers from the input text
-parseRule1 :: String -> Rule
-parseRule1 str = (src, dst, len)
+parseRule :: String -> Rule
+parseRule str = (src, dst, len)
   where
     (src, dst, len) = (numbers !! 1, numbers !! 0, numbers !! 2)
     numbers         = map readInt . tok " " $ str
 
 -- Look up the corresponding output to the input x in a Table of Rule-s
-lookup1 :: Table -> Int -> Int
-lookup1 [] x = x
-lookup1 ((src, dst, len):ms) x
+lookup' :: Table -> Int -> Int
+lookup' [] x = x
+lookup' ((src, dst, len):ms) x
     |(x >= src) && (x < src+len) = dst + x - src
-    |otherwise                   = lookup1 ms x
+    |otherwise                   = lookup' ms x
 
 -- Extract seed numbers from the first line of input text
-getSeeds1 :: [String] -> [Int]
-getSeeds1 input = map readInt . tok " " . at 1 . tok ":" . at 0 $ input
-
-------------
--- Part 2 --
-------------
-
--- Extract seed numbers from the first line of input text
+getSeeds :: [String] -> [Int]
+getSeeds input = map readInt . tok " " . at 1 . tok ":" . at 0 $ input
 
 -------------
 -- Answers --
@@ -44,29 +31,25 @@ main = do
     filecontents <- readFile "input.txt"
 
     -- Part 1
-    let seeds = getSeeds1 . lines $ filecontents -- part 1
+    let seeds = getSeeds . lines $ filecontents -- part 1
 
-    let tables = map (map parseRule1)            -- parse every tables' rules
+    let tables = map (map parseRule)            -- parse every tables' rules
                . map tail                        -- drop table headings
                . tok [""]                        -- separate tables
                . drop 2                          -- drop top two lines (seeds)
                . lines $ filecontents            -- split up file lines
     
-    let locations = map (lookup1 (tables!!6))    -- Applying the transformation tables
-                  . map (lookup1 (tables!!5))
-                  . map (lookup1 (tables!!4))
-                  . map (lookup1 (tables!!3))
-                  . map (lookup1 (tables!!2))
-                  . map (lookup1 (tables!!1))
-                  . map (lookup1 (tables!!0))
+    let locations = map (lookup' (tables!!6))    -- Applying the transformation tables
+                  . map (lookup' (tables!!5))
+                  . map (lookup' (tables!!4))
+                  . map (lookup' (tables!!3))
+                  . map (lookup' (tables!!2))
+                  . map (lookup' (tables!!1))
+                  . map (lookup' (tables!!0))
                   $ seeds
 
 
     print $ minimum locations
-
-    -- Part 2
-    -- t.b.d. efficiently
-
 
 
     print $ "---------- Done. ----------"
