2412755732c6df56ee5f3ad6126d51f97286c6e9
Matthias M. Fischer <33027321+Ma-Fi-94@users.noreply.github.com>
2023-12-05 21:22:36 +0100

    bit of cleanup

--- day04/solution.hs
+++ day04/solution.hs
@@ -25,7 +25,7 @@ parseLine s = (wins, numbers)
 
 -- Find the number of matching numbers on a Card.
 nbMatches :: Card -> Int
-nbMatches (wins, numbers) = length $ intersect wins numbers
+nbMatches = length . uncurry intersect
 
 
 ------------
@@ -35,12 +35,11 @@ nbMatches (wins, numbers) = length $ intersect wins numbers
 
 -- Calculate the score of a given card.
 scoreCard :: Card -> Int
-scoreCard (wins, numbers)
-    |n == 1    = 1
-    |n > 1     = 2 ^ (n - 1)
+scoreCard card
+    |m >= 1    = 2 ^ (m - 1)
     |otherwise = 0
   where
-    n = nbMatches (wins, numbers)
+    m = nbMatches card
     
 
 ------------
@@ -49,7 +48,7 @@ scoreCard (wins, numbers)
 
 
 -- Process the first card of a given stash of cards.
--- - We drop the first care
+-- - We drop the first card
 -- - The count of the next nbMatches cards are increased by nbOwned.
 -- - The remaining cards are returned unchanged.
 -- We don't have to worry about "overshooting", as Prelude.take
@@ -83,11 +82,7 @@ main = do
     -- We step over the stash of cards from top to bottom
     -- Because step [] = [], we get an infinite list,
     -- so we only take non-empty lists.
-    -- (I know this is kinda hacky and there prolly is a better way.)
     let steps = takeWhile (/=[]) $ iterate step stash
-
-    -- Prettyprinting. This will produce a lot of output.
-    -- mapM_ print steps
 
     -- The final answer is the sum of the nbOwned of all first elements
     print $ sum . map snd . map head $ steps
