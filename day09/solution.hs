d2f602a75a252f891d0eaac8a301f9c3a6e0d9d9
Matthias M. Fischer <33027321+Ma-Fi-94@users.noreply.github.com>
2023-12-09 13:03:51 +0100

    Folding to make it a bit more terse

--- day09/solution.hs
+++ day09/solution.hs
@@ -11,21 +11,20 @@ parseLine = map readInteger . tok " "
 
 -- Calculate iteratively the differences between subsequent numbers,
 -- until we reach a list of all zeros. Returned list of lists includes
--- initial input, but not the final zeros, and is reversed for convenience.
-diffsRev :: (Num a, Eq a) => [a] -> [[a]]
-diffsRev = reverse . takeWhile (any (/=0)) . iterate diff
+-- initial input, but not the final zeros.
+diffs :: (Num a, Eq a) => [a] -> [[a]]
+diffs = takeWhile (any (/=0)) . iterate diff
   where
     diff xs = zipWith (-) (tail xs) (init xs)
 
 
 -- The actual prediction of the next element of a given list.
+-- We calculate the lists of differences, reverse it, and then
+-- step through them, filling in and carrying over the respective last element.
 predictNext :: (Num a, Eq a) => [a] -> a
-predictNext = go 0 . diffsRev 
+predictNext = foldl step 0 . reverse . diffs 
   where
-    go delta (l:[]) = (last l) + delta
-    go delta (l:ls) = go delta' ls
-      where
-        delta' = (last l) + delta
+    step delta xs = delta + (last xs)
 
 
 -------------
