9e5211cab57b564f51b38d3dba05737fc4c9d3b7
Matthias M. Fischer <33027321+Ma-Fi-94@users.noreply.github.com>
2023-12-11 18:57:17 +0100

    Layouting

--- day11/solution.hs
+++ day11/solution.hs
@@ -1,23 +1,22 @@
 import Data.List (elemIndices, (\\), sortOn)
 
 
------------
--- Sugar --
------------
-
+-- Sugar
 type Coord = (Int, Int)
 
-
----------------
--- Functions --
----------------
 
 -- Uncurried taxicab metric
 cab :: (Coord, Coord) -> Int
 cab ((x1, y1), (x2, y2)) = abs (x1-x2) + abs (y1-y2)
 
 
--- Extract coordinates of non-empty cells iteratively row by row
+-- All pairs of the elements of a list
+pairs :: Eq a => [a] -> [(a,a)]
+pairs xs = [(x1, x2) | x1 <- xs, x2 <- xs]
+
+
+-- Converts input file's lines to a list of coordinates.
+-- We do this in the very beginning for optimal performance.
 lines2coords :: [String] -> [Coord]
 lines2coords = go 0
   where
@@ -26,8 +25,6 @@ lines2coords = go 0
 
 
 -- Expand the "grid" row-wise by adding in 'n' new rows per empty row.
--- We operate on the list of coordinates already, as this
--- is more efficient than working with a character grid in memory.
 expandRowsN :: Int -> [Coord] -> [Coord]
 expandRowsN n = go 0 0
   where
@@ -49,13 +46,6 @@ expandN n = transpose . expandRowsN n . transpose . expandRowsN n
     transpose = sortOn fst . map (\(x,y) -> (y,x))
 
 
--- All non-trivial pairs of the elements of a list
-pairs :: Eq a => [a] -> [(a,a)]
-pairs xs = [(x1, x2) | x1 <- xs,
-                       x2 <- xs,
-                       x1 /= x2]
-
-
 -------------
 -- Answers --
 -------------
@@ -65,16 +55,10 @@ main = do
     filecontents <- readFile "input.txt"
     let coords = lines2coords . lines $ filecontents
 
-    ------------
-    -- Part 1 --
-    ------------
-
+    -- Part 1
     print $ (`div` 2) . sum . map cab . pairs . expandN 1 $ coords
 
-    ------------
-    -- Part 2 --
-    ------------
-
+    -- Part 2
     print $ (`div` 2) . sum . map cab . pairs . expandN (1000000-1) $ coords
 
 
