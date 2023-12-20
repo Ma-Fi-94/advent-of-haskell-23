eecff4682e061ede0f7de5e8a69cd576f2dc9c28
Matthias M. Fischer <33027321+Ma-Fi-94@users.noreply.github.com>
2023-12-20 18:31:53 +0100

    Add files via upload

---
+++ day20/solution.hs
@@ -0,0 +1,3 @@
+-- I used the solution in https://github.com/Sheinxy/Advent-Of-Code/blob/main/2023/Day_20/Day_20.hs
+-- and I still want to properly understand and annotate it, before I build upon it to make my own.
+-- TBD.
